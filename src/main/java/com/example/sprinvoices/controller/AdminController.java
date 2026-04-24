cdb875dc88a8ac511b253f3dad31cab3c907fb44
koffi-samuela <samukoffi123@gmail.com>
2026-04-24 09:34:03 +0200

    fix pagination

--- src/main/java/com/example/sprinvoices/controller/AdminController.java
+++ src/main/java/com/example/sprinvoices/controller/AdminController.java
@@ -174,7 +174,7 @@ public String invoices(
         @RequestParam(defaultValue = "0") int page,
         Model model) {
 
-    Pageable pageable = PageRequest.of(page, 10);
+    Pageable pageable = PageRequest.of(page, 1); // 1 facture par page pour tester
 
     Page<Invoice> invoices = invoiceService.findWithFilters(
             customerId,
